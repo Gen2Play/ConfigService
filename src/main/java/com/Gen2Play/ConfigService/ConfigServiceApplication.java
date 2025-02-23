0cacbcf5dfa06e3a8ceff3b1ef4cdd3d72027662
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-23 23:43:41 +0700

    fix bug env

--- src/main/java/com/Gen2Play/ConfigService/ConfigServiceApplication.java
+++ src/main/java/com/Gen2Play/ConfigService/ConfigServiceApplication.java
@@ -4,16 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableConfigServer
-// @EnableAutoConfiguration(exclude = VaultAutoConfiguration.class)
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// Dotenv dotenv = Dotenv.load();
+		// dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
