984f8761e6c9a899aea3132f9338675e4313ccdf
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-23 23:27:40 +0700

    add Dockerfile to deploy by Railway

--- src/main/java/com/Gen2Play/ConfigService/ConfigServiceApplication.java
+++ src/main/java/com/Gen2Play/ConfigService/ConfigServiceApplication.java
@@ -1,16 +1,14 @@
 package com.Gen2Play.ConfigService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.vault.config.VaultAutoConfiguration;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableAutoConfiguration(exclude = VaultAutoConfiguration.class)
+// @EnableAutoConfiguration(exclude = VaultAutoConfiguration.class)
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
